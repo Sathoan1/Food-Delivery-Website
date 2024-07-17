28815171d41d6cdc41d4aed37f5e9c314133b0c9
Sathoan1 <sathoanoamen@gmail.com>
2024-07-17 14:56:03 -0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Delicious Meals Delivered Fast.</h2>
+            <p>Experience the best of local cuisine, delivered straight to your door. Order now and enjoy fresh, hot meals anytime, anywhere.</p>
+            <button>View Menu</button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+
+import { assets } from '../../assets/assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iste asperiores voluptas? Incidunt, magnam</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+           <h2>COMPANY</h2>
+           <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privact Policy</li>
+           </ul>
+        </div>
+        <div className="footer-content-right">
+        <h2>GET IN TOUCH</h2>
+        <ul>
+            <li>+234-806-767-3434</li>
+            <li>contact@tomato.com</li>
+        </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>
+        Copyright 2024 Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
